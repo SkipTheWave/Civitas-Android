5758cc9498875c094139628ea145d4d48bef6942
Jose <jn.cunha@campus.fct.unl.pt>
2022-05-08 16:31:55 +0100

    Almost fixed Register form observer.

--- app/src/main/java/pt/unl/fct/civitas/ui/register/RegisterViewModel.java
+++ app/src/main/java/pt/unl/fct/civitas/ui/register/RegisterViewModel.java
@@ -56,10 +56,10 @@ public class RegisterViewModel extends ViewModel {
             registerFormState.setValue(new RegisterFormState(null, R.string.invalid_password, null, null, null));
         } else if (!passwordsMatch(password, confirmPassword)) {
             registerFormState.setValue(new RegisterFormState(null, null, R.string.error_different_passwords, null, null));
+        } else if (!isNameValid(name)) {
+            registerFormState.setValue(new RegisterFormState(null, R.string.invalid_name, null, null, null));
         } else if (!isEmailValid(email)) {
             registerFormState.setValue(new RegisterFormState(null, null, null, R.string.invalid_email, null));
-        } else if (!isNameValid(name)) {
-            registerFormState.setValue(new RegisterFormState(null, null, null, null, R.string.invalid_name));
         }
     }
 
